b32ad16746f479f9338bf175339bc371a84222d5
vuthaihoa <thaihoa290999@gmail.com>
2023-02-03 16:06:56 +0700

    3/2/2023

--- Assets/_Scripts/Player/PlayerCombo.cs
+++ Assets/_Scripts/Player/PlayerCombo.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Localization.Plugins.XLIFF.V12;
+//using UnityEditor.Localization.Plugins.XLIFF.V12;
 using UnityEngine;
 
 public class PlayerCombo : MonoBehaviour
